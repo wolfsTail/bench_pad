80e6423354bd4218388974ca8c313a5cfbfe835a
DRomanov <lukaviy2la@gmail.com>
2023-08-10 20:44:30 +0300

    Start version 0.1.0

---
+++ dj_project_bench_pad/dj_project_bench_pad/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for dj_project_bench_pad project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dj_project_bench_pad.settings')
+
+application = get_asgi_application()
