29f44f8c6eb365b2357331f4f6c7eddaa8035513
DRomanov <lukaviy2la@gmail.com>
2023-08-15 20:57:21 +0300

    Version 0.2.0; add new view, fix some bugs

--- dj_project_bench_pad/dj_app_blog/models.py
+++ dj_project_bench_pad/dj_app_blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 # Create your models here.
@@ -15,7 +16,7 @@ class Post(models.Model):
         PUBLISHED = "PB", "Опубликовано"
 
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250)
+    slug = models.SlugField(max_length=250, unique_for_date='publish')
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="blog_posts")
     body = models.TextField()
@@ -32,3 +33,9 @@ class Post(models.Model):
 
     def __str__(self):
         return f"{self.publish}: {self.title}"
+
+    def get_absolute_url(self):
+        return reverse("blog:post_detail", args=[self.publish.year,
+                                                 self.publish.month,
+                                                 self.publish.day,
+                                                 self.slug])
--- dj_project_bench_pad/dj_app_blog/views.py
+++ dj_project_bench_pad/dj_app_blog/views.py
@@ -10,6 +10,13 @@ def post_list(request):
     return render(request, "blog/post/list.html", {"posts": posts})
 
 
-def post_deatail(request, id):
-    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
-    return render(request, 'blog/post/detail.html', {'post': post})
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post,
+                             status=Post.Status.PUBLISHED,
+                             slug=post,
+                             publish__year=year,
+                             publish__month=month,
+                             publish__day=day)
+    return render(request,
+                  'blog/post/details.html',
+                  {'post': post})
