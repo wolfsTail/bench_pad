78215f4311563684c2ba631fd2799526a2570f0b
wolfsTail <129546782+wolfsTail@users.noreply.github.com>
2023-08-19 18:55:36 +0300

    Update settings.py
    ,del personal information.

--- dj_project_bench_pad/dj_project_bench_pad/settings.py
+++ dj_project_bench_pad/dj_project_bench_pad/settings.py
@@ -128,8 +128,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 # конфигурация сервера электронной почты
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'lukaviy2la@gmail.com'
-EMAIL_HOST_PASSWORD = 'yxxqolntsxqqaqbp'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+# данные удалены!!!
+EMAIL_HOST = 
+EMAIL_HOST_USER =
+EMAIL_HOST_PASSWORD = 
+EMAIL_PORT = 
+EMAIL_USE_TLS = 
